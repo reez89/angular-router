b45a08788323fe89449d08246a82f1c45eded36e
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-18 19:46:10 +0200

    aggiunto subscribe alla route per ottenere il cambiamento dei parametri

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,15 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.route.params // params è un Observable che ci aiuta a lavoare con async app.
+    .subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+        }
+        );
+        // tslint:disable-next-line: max-line-length
+       // abbiamo aggiunto questo nuovo metodo, perchè con il pulsante load Anna, non richiamavamo i parametri corretti.In questo modo ad ogni cambiamento dei parametri, il subscribe cambieraà di conseguenza id e name all'interno dell'html.
   }
 
 }
