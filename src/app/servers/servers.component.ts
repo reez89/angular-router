2243d4f62ffda58852ee9eb496babc121a92596b
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-18 19:22:00 +0200

    aggiunto nuovo metodo per ricaricare una pagina, per ora commentato

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    /* this.router.navigate(['servers'],{relativeTo: this.route}); */
+    // tslint:disable-next-line: max-line-length
+    // il metodo navigate non sa dove si trova, quindi anche scrivendo il path in quel modo, non restituirà un'errore. Ecco perchè utilizziamo la proprietà relativeTo !
   }
 
 }
