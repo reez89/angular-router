5ddba8452c7cf87fd99e4556c05a6a84c639634a
NIcola R <nicola.rizzi89@hotmail.it>
2021-04-24 11:33:47 +0200

    aggiunto resolver per gestire la navigazione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent }, // localhost:4200/
+  { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent }, // localhost:4200/userid
-  ] }, // localhost:4200/users
+    { path: ':id/:name', component: UserComponent },
+  ] },
   { path: 'servers',
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-    { path: ':id', component: ServerComponent }, // localhost:4200/servers
-    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }, // localhost:4200/servers/edit
-  ] }, // localhost:4200/servers
-  { path: 'not-found', component: PageNotFoundComponent},
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
+  ] },
+  // { path: 'not-found', component: PageNotFoundComponent},
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not Found!'}},
   { path: '**', redirectTo: '/not-found'} // wildcard, prende tutto quello che non conosce, e si posiziona sempre alla fine!!!
 ];
 @NgModule({
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,14 +16,21 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
+
+    this.route.data
+        .subscribe(
+          (data: Data) => {
+            this.server = data['server'] //è il nome che abbiamo assegnato noi nella route.
+          }
+        );
+    /* const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params
       .subscribe(
         (params: Params) => {
         this.server = this.serversService.getServer(+params['id']);
       }
-    );
+    ); */
   }
 
   onEdit(){
